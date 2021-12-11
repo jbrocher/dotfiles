8e0c502c4b8cb9e90b91234e0a642fa80791d0bc
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-12-11 12:46:18 +0100

    install svelte lsp

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -238,7 +238,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Enable the following language servers
-local servers = { 'pyright'}
+local servers = { 'pyright', 'svelte'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
