66e80620d13ef6b5773b7f29da745f41cffeaa4e
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2022-04-08 11:14:31 +0200

    fix: Correct completopt config

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -358,21 +358,42 @@ vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.o.foldlevel = 99
 
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noinsert'
 
 -- Compe setup
-require('compe').setup {
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  resolve_timeout = 800;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
+
   source = {
-    path = true,
-    nvim_lsp = true,
-    buffer = false,
-    calc = false,
-    nvim_lua = false,
-    vsnip = false,
-    ultisnips = false,
-  },
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    vsnip = true;
+    ultisnips = true;
+  };
 }
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noinsert'
 
 -- Utility functions for compe 
 local t = function(str)
