fb4f29d41379efb8e434507dcdceb7308de05b6d
Murfy JB <jean-baptiste@murfy.fr>
2022-08-08 11:37:13 +0200

    feat: Install pylint lsp

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -259,9 +259,43 @@ filetypes = {
     typescript = "eslint",
     javascript = "eslint",
     typescriptreact = "eslint",
+    python ="pylint"
 }
 
 local linters = {
+    pylint= {
+      sourceName= "pylint",
+      command= "pylint",
+      args= {
+        "--output-format",
+        "text",
+        "--score",
+        "no",
+        "--msg-template",
+        "'{line}:{column}:{category}:{msg} ({msg_id}:{symbol})'",
+        "%file"
+      },
+      formatPattern= {
+        "^(\\d+?):(\\d+?):([a-z]+?):(.*)$",
+        {
+          line= 1,
+          column= 2,
+          security= 3,
+          message= 4
+        }
+      },
+      rootPatterns= {".git", "pyproject.toml", "setup.py"},
+      securities= {
+        informational= "hint",
+        refactor= "info",
+        convention= "info",
+        warning= "warning",
+        error= "error",
+        fatal= "error"
+      },
+      offsetColumn= 1,
+      formatLines= 1
+    },
     eslint = {
         sourceName = "eslint",
         command = "eslint",
